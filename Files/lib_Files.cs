0958452c6aefbe55f6e8ed834e7e0968152ce7bd
junkfreaknetyz <junk.freak.net.yz@gmail.com>
2020-10-12 09:52:08 +0900

    2020/10/12 09:51

--- Files/lib_Files.cs
+++ Files/lib_Files.cs
@@ -3,12 +3,19 @@ using System;
 namespace myAure.files
 {
     //
+    //connection string class
+    //for connecting to azure storage
+    //
     public class ConnectionString
     {
         private String connection_String;
         public ConnectionString(String para)
         {
             this.connection_String = para;
+        }
+        public ConnectionString(ConnectionString para)
+        {
+            this.connection_String=para.Get();
         }
         public String Get(){
             return this.connection_String;
@@ -17,13 +24,22 @@ namespace myAure.files
         {
             this.connection_String=para;
         }
+        public void Set(ConnectionString para)
+        {
+            this.connection_String=para.Get();
+        }
     }
+    //share name class
     public class ShareName
     {
         private String share_Name;
         public ShareName(String para)
         {
             this.share_Name=para;
+        }
+        public ShareName(ShareName para)
+        {
+            this.share_Name=para.Get();
         }
         public String Get()
         {
@@ -32,11 +48,32 @@ namespace myAure.files
         public void Set(String para)
         {
             this.share_Name=para;
-        }        
+        }
+        public void Set(ShareName para)
+        {
+            this.share_Name=para.Get();
+        }
     }
-    //public class FilesDirectory
-    //{
+    public class FilesDirectory
+    {
 
-    //}
+        private String files_directory;
+        public FilesDirectory(FilesDirectory para)
+        {
+            this.files_directory=para.Get();
+        }
+        public FilesDirectory(String para)
+        {
+            this.files_directory=para;
+        }
+        public void Set(String para)
+        {
+            this.files_directory=para;
+        }
+        public String Get()
+        {
+            return this.files_directory;
+        }
+    }
     
 }
