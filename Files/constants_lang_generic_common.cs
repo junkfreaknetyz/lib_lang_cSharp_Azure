fdc9093fec75e55e8e0ed7754eba7378839e605c
junkfreaknetyz <junk.freak.net.yz@gmail.com>
2020-11-01 09:41:27 +0900

    2020/11/01 09:40

--- Files/constants_lang_generic_common.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace language.generic.common {
-    public static class constants {
-        public static readonly string CS_SPACE="";
-        public static readonly int CS_ZERO_AS_INT=0;
-        public static readonly string CS_ZERO_AS_STRING="0";
-    }   
-}
