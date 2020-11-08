74669532a1b833439285ab07b74ec6b5b56bcee7
junkfreaknetyz <junk.freak.net.yz@gmail.com>
2020-11-08 10:09:43 +0900

    2020/11/08 10:09

--- Files/Files.cs
+++ Files/Files.cs
@@ -8,7 +8,7 @@ using language.generic.common;
 
 namespace language.azure.files
 {
-    public static class Files
+    public static class Func
     {
         //  upload file
         public static void Upload(ConnectionString connection_string,ShareName share_name,FilesDirectory files_directory,FilesName files_name,language.generic.common.io.Directory directory_from,language.generic.common.io.File file_from,language.generic.common.io.PathSeparatore path_separatore)
@@ -39,8 +39,16 @@ namespace language.azure.files
     //
     //
     class test{
-        static void Main(AssemblyLoadEventArgs[]){
+        static void Main(String[] args){
 
+            String strCnnctnToAzure="DefaultEndpointsProtocol=https;AccountName=yzloglarge;AccountKey=bvfsXft7QcGaD19DiZrbIMBO/NFMk5egZrvtNlQOC1cnyP/f9Y0tLStGq6AhKGPxoVCxFkQqSVxEHyt77FOHNw==;EndpointSuffix=core.windows.net";
+            String strShareNameToAzure="shrstrgyzlogistics";
+            String strFilesDirectory=".";
+            String strFileName="dbo_TBL_OUTFILE.txt";
+            String strDirectoryFrom=".";
+            String strFileNameFrom="dbo_TBL_OUTFILE.txt";
+
+            Func.Upload(new ConnectionString(strCnnctnToAzure),new ShareName(strShareNameToAzure),new FilesDirectory(strFilesDirectory),new FilesName(strFileName),new generic.common.io.Directory(strDirectoryFrom),new generic.common.io.File(strFileNameFrom),new generic.common.io.PathSeparatore("/"));
         }
     }
 
